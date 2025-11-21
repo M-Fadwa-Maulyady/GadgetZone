7fad6fe7a760bd96d19f1c0db1c4163c0e63a8e1
M-Fadwa-Maulyady <maulyadyfadwa@gmail.com>
2025-11-21 19:30:00 +0700

    layoutuser

---
+++ resources/views/components/layoutUser.blade.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width,initial-scale=1" />
+  <title>GadgetZone — Demo</title>
+  <link rel="stylesheet" href="{{ asset('tema/style.css') }}" />
+</head>
+<body>
+  
+    <x-navbarUser></x-navbarUser>
+        <main>
+         {{ $slot }}
+        </main>
+
+    <x-footerUser></x-footerUser>
+
+  <script src="{{ asset('tema/script.js') }}"></script>
+</body>
+</html>
